519bd6af05d6a62679ee85a1d8b39eca681e81dd
anrothis <riedel-sebastian@t-online.de>
2021-04-19 19:20:22 +0200

    added FileChooser to Import

--- src/main/java/de/trs/javafx/memberview/ImportSceneController.java
+++ src/main/java/de/trs/javafx/memberview/ImportSceneController.java
@@ -1,14 +1,20 @@
 package de.trs.javafx.memberview;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
+@Controller
 public class ImportSceneController {
 
     @FXML
@@ -32,24 +38,25 @@ public class ImportSceneController {
     @FXML
     private Button importButton;
 
+    private FileChooser fileChooser;
+
     @FXML
     void initialize() {
-        // assert filePathTextField != null
-        // : "fx:id=\"filePathTextField\" was not injected: check your FXML file
-        // 'ImportScene.fxml'.";
-        // assert lookupButton != null
-        // : "fx:id=\"lookupButton\" was not injected: check your FXML file
-        // 'ImportScene.fxml'.";
-        // assert loadButton != null : "fx:id=\"loadButton\" was not injected: check
-        // your FXML file 'ImportScene.fxml'.";
-        // assert previewListView != null
-        // : "fx:id=\"previewListView\" was not injected: check your FXML file
-        // 'ImportScene.fxml'.";
-        // assert importButton != null
-        // : "fx:id=\"importButton\" was not injected: check your FXML file
-        // 'ImportScene.fxml'.";
-        loadButton.setOnAction(event -> {
-            new FileChooser();
+
+        lookupButton.setOnAction(event -> {
+            Stage mainStage = (Stage) javafx.stage.Window.getWindows().get(0);
+            fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.getExtensionFilters().addAll(new ExtensionFilter("CSV Files", "*.csv"),
+                    new ExtensionFilter("Text Files", "*.txt"),
+                    new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                    new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                    new ExtensionFilter("All Files", "*.*"));
+            File selectedFile = fileChooser.showOpenDialog(mainStage);
+            if (selectedFile != null) {
+                // mainStage.display(selectedFile);
+                filePathTextField.setText(selectedFile.getAbsolutePath());
+            }
 
         });
     }
--- src/main/java/de/trs/javafx/memberview/SwitchScene.java
+++ src/main/java/de/trs/javafx/memberview/SwitchScene.java
@@ -2,7 +2,7 @@ package de.trs.javafx.memberview;
 
 public enum SwitchScene {
     TABLEVIEW("/fxml/StageScene.fxml"), SEARCHVIEW("/fxml/SearchScene.fxml"), ADDVIEW("/fxml/AddScene.fxml"),
-    IMPORTVIEW("/fxml/ImportStage.fxml");
+    IMPORTVIEW("/fxml/ImportScene.fxml");
 
     private final String fxmlPath;
 
