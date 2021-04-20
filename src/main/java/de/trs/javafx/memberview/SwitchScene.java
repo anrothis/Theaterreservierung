aeb484952d392ff02944a9cf5f11733ca9e5641f
anrothis <riedel-sebastian@t-online.de>
2021-04-20 08:17:55 +0200

    updat

---
+++ src/main/java/de/trs/javafx/memberview/SwitchScene.java
@@ -0,0 +1,22 @@
+package de.trs.javafx.memberview;
+
+public enum SwitchScene {
+    TABLEVIEW("/fxml/StageScene.fxml"),
+    
+    SEARCHVIEW("/fxml/SearchScene.fxml"),
+    
+    ADDVIEW("/fxml/AddScene.fxml"),
+
+    IMPORTVIEW("/fxml/ImportScene.fxml");
+
+    private final String fxmlPath;
+    
+    private SwitchScene(String fxmlPath) {
+        this.fxmlPath = fxmlPath;
+    }
+    
+    public String getFxml() {
+        return fxmlPath;
+    }
+
+}
